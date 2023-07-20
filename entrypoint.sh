c603ec4a69f4bceeaef243a3686b941617b5c015
Anna BIllstrom <banane@gmail.com>
2023-07-20 10:15:05 -0700

    rolled back all but post to tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,30 +11,10 @@ if [[ -z "${GITHUB_TOKEN}" ]]; then
    exit 1
 fi
 
-if [[ -z "${GITHUB_ACTOR}" ]]; then 
-   echo "No actor supplied"
-   exit 1
-fi
-
-if [[ -z "$GITHUB_HEAD_REF}" ]]; then
-   echo "No ref supplied"
-   exit 1
-fi
-
-if [[ -z "$GITHUB_BASE_REF}" ]]; then
-   echo "No ref supplied"
-   exit 1
-fi
-
-if [[ -z "$GITHUB_HEAD_SHA}" ]]; then 
-   echo "No head sha supplied"
-   exit 1
-fi
-
-
 # get GitHub API endpoints prefix
 git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
 git_tags_url=$(jq .repository.git_tags_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
+
 
 # check if tag already exists in the cloned repo
 tag_exists="false"
@@ -53,21 +33,32 @@ fi
 
 echo "**pushing tag $TAG to repo $GITHUB_REPOSITORY"
 
-
-
 if $tag_exists
 then
-# create new tag
-  echo "git tags url: $git_tags_url"
-  echo "$GITHUB_HEAD_SHA github head sha"
-  curl -X POST "$git_tags_url" \
+
+  # update tag
+  curl -s -X PATCH "$git_refs_url/tags/$TAG" \
   -H "Authorization: token $GITHUB_TOKEN" \
   -d @- << EOF
+
   {
-    "tag": "$TAG",
-    "object": "$GITHUB_HEAD_SHA",
-    "message":"$GITHUB_ACTOR updated PR $GITHUB_HEAD_REF to $GITHUB_BASE_REF",
-    "type": "commit"
+    "sha": "$GITHUB_SHA",
+    "force": true
   }
 EOF
+else
+  # create new tag
+  echo "tag: $TAG"
+output=`curl -s -X POST "$git_tags_url" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -d @- << EOF
+
+  {
+     "tag": "v.3.86.1005",
+     "object": "$GITHUB_HEAD_SHA",
+     "message":"$GITHUB_ACTOR updated PR $GITHUB_HEAD_REF to $GITHUB_BASE_REF",
+     "type": "commit"
+  }
+EOF`
+echo "$output"
 fi
