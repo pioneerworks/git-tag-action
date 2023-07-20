7f40e29e813e1f24fb460264872cdb333facec17
Anna BIllstrom <banane@gmail.com>
2023-07-19 20:51:46 -0700

    Adding in message with tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,27 @@ if [[ -z "${GITHUB_TOKEN}" ]]; then
    exit 1
 fi
 
+if [[ -z "${GITHUB_ACTOR}" ]]; then 
+   echo "No actor supplied"
+   exit 1
+fi
+
+if [[ -z "$GITHUB_HEAD_REF}" ]]; then
+   echo "No ref supplied"
+   exit 1
+fi
+
+if [[ -z "$GITHUB_BASE_REF}" ]]; then
+   echo "No ref supplied"
+   exit 1
+fi
+
+if [[ -z "$GITHUB_HEAD_SHA}" ]]; then 
+   echo "No head sha supplied"
+   exit 1
+fi
+
+
 # get GitHub API endpoints prefix
 git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
 git_tags_url=$(jq .repository.git_tags_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
@@ -32,31 +53,21 @@ fi
 
 echo "**pushing tag $TAG to repo $GITHUB_REPOSITORY"
 
+
+
 if $tag_exists
 then
-  # update tag
-  curl -s -X PATCH "$git_refs_url/tags/$TAG" \
+# create new tag
+  echo "git tags url: $git_tags_url"
+  echo "$GITHUB_HEAD_SHA github head sha"
+  curl -X POST "$git_tags_url" \
   -H "Authorization: token $GITHUB_TOKEN" \
   -d @- << EOF
-
-  {
-    "sha": "$GITHUB_SHA",
-    "force": true
-  }
-EOF
-else
-  # create new tag
-  echo "creating new tag, ref: $git_refs_url"
-  echo "tags url: $git_tags_url"
-  echo "github context: $GITHUB_CONTEXT"
-  curl -s -X POST "$git_tags_url" \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  -d @- << EOF
-
   {
     "tag": "$TAG",
-    "object": "$GITHUB_SHA",
-    "message":"abc" 
+    "object": "$GITHUB_HEAD_SHA",
+    "message":"$GITHUB_ACTOR updated PR $GITHUB_HEAD_REF to $GITHUB_BASE_REF",
+    "type": "commit"
   }
 EOF
 fi
